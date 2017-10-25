6dc533da656e2c2d201c479b94f77546bc305656
Douglas Alves <douglas.expalves@gmail.com>
2017-10-25 20:43:41 -0200

    name change and new example

--- twitte.r
+++ twitte.r
@@ -10,6 +10,12 @@ setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
 # woeid -> where on earth id
 # 455819 é o código de Brasília
-trendsBrasilia <- getTrends(woeid = 455819)
+# 455827 é o código de São Paulo
+trendsBrasilia <- getTrends(woeid = 455827)
 # 10 primeiros apenas
 trendsBrasilia$name[1:10]
+
+imp <- searchTwitter('Wenger', n = 50)  #COYG lol
+imp[c(1, 25, 50)]
+
+tweet("...")
